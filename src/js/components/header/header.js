30d0f83a396fb173a66b6f49230e2b55af2543c3
Cerneaga Denis <prolevelmd@gmail.com>
2020-04-03 19:29:25 +0300

    Add homework for lesson 20

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -5,18 +5,22 @@ const headerContainer = document.getElementById('header-hbs');
 /* logic with nav links */
 let navigationLinks = [
   {
+    linkNumber: 'n1',
     url: '/',
     label: 'Home',
   },
   {
+    linkNumber: 'n2',
     url: '/about-us',
     label: 'About',
   },
   {
+    linkNumber: 'n3',
     url: '/products',
     label: 'Products',
   },
   {
+    linkNumber: 'n4',
     url: '/contact-us',
     label: 'Contacts',
   },
